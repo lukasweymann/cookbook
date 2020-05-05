389fc14f7a0711debdc5755a440a419abe8c2326
Kristian Hetherington <hetheringtonkristian@gmail.com>
2020-05-05 16:50:49 +0200

    fixed random recipes functionality on all pages

--- cookbook-master/landing pages/index.js
+++ cookbook-master/landing pages/index.js
@@ -2,5 +2,25 @@ function mySurprise() {
 
     var randomRecipe = Math.round(Math.random() * 11) + 1;
 
+    
+
     window.location.href = `cookbook-master/recipes/recipe${randomRecipe}.html`;
+}
+
+function mySurpriseRecipe() {
+
+    var randomRecipe = Math.round(Math.random() * 11) + 1;
+
+
+
+    window.location.href = `recipe${randomRecipe}.html`;
+}
+
+function mySurpriseLandingPages() {
+
+    var randomRecipe = Math.round(Math.random() * 11) + 1;
+
+
+
+    window.location.href = `../recipes/recipe${randomRecipe}.html`;
 }
